d58aabebf1308df12facbdfebf12ebbb88595438
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-10-03 13:46:47 +0100

    progress

--- LLM.py
+++ LLM.py
@@ -4,12 +4,20 @@ from huggingface_hub import InferenceClient
 load_dotenv()
 
 
-MODEL = "meta-llama/Meta-Llama-3.1-70B-Instruct"
+# MODEL = "meta-llama/Meta-Llama-3.1-70B-Instruct"
+MODEL = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 KEY = os.getenv("KEY")
 
 class LLM:
+    """
+    A class to interact with the LLM model
+    """
 
-    def __init__(self, model=MODEL, key=KEY):
+    def __init__(
+        self, 
+        model=MODEL, 
+        key=KEY
+    ):
         self.model = model
         self.key = key
 
@@ -29,7 +37,7 @@ class LLM:
 	        { "role": "user", "content": query }
         ]
         output = client.chat.completions.create(
-            model="meta-llama/Llama-3.1-70B-Instruct", 
+            model=self.model,
             messages=messages, 
             stream=True, 
             temperature=0.5,
@@ -40,5 +48,44 @@ class LLM:
             chunk.choices[0].delta.content for chunk in output
         )
         return answer
+    
+
+    def transcript_to_query(self, transcripts: list) -> str:
+        """
+        Convert a list of transcripts to a query
+
+        Args:
+            transcripts (list): A list of transcripts
+
+        Returns:
+            str: The query to ask the model
+        """
+        text = " ".join(transcripts)
+        query = (
+            "Preciso de um titulo chamativo, resumo em formato expositivo e 3 keywords para a seguinte transcrição de um clipe de um programa de TV:\n\n" +  
+            f"{text}\n\n" +
+            "A resposta deve ser em português de portugal e ter o seguinte formato: \n Titulo: ... \n Resumo: ... \n Keywords: ..."
+        )
+        return query
+    
+
+    def process_transcripts(self, transcripts: list) -> str:
+        """
+        Process a list of transcripts
+
+        Args:
+            transcripts (list): A list of transcripts
+
+        Returns:
+            Tuple[str, str, List[str]]: Title, summary, and keywords for the transcripts
+        """
+        query = self.transcript_to_query(transcripts)
+        response = self.query_cloud(query)
+        response = response.split("\n")
+        response = [line.strip() for line in response if line]
+        title = response[0][8:]
+        summary = response[1][8:]
+        keywords = response[2][10:].split(",")
+        return title, summary, keywords
 
     
---
+++ test.py
@@ -0,0 +1,14 @@
+from LLM import LLM
+
+llm = LLM()
+
+transcripts = [
+    "Sejam muito bem-vindos em destaque esta semana à Assembleia Geral das Nações Unidas em Nova Iorque, uma missão militar europeia que acompanhamos em Moçambique.",
+    "E ainda, conversamos com os realizadores finalistas do Prémio Europeu de Cinema Luz."
+    "Começamos com os principais destaques desta semana. A Assembleia Geral das Nações Unidas reunida em Nova Iorque. O presidente ucraniano foi mais uma vez fazer apelos à paz e avisar para os perigos de um desastre nuclear. Destaca ainda para a França o novo governo reuniu pela primeira vez."
+]
+
+title, summary, keywords = llm.transcript_to_query(transcripts)
+print(f"\nTitle: \n{title}")
+print(f"\nSummary: \n{summary}")
+print(f"\nKeywords: \n{keywords}")
