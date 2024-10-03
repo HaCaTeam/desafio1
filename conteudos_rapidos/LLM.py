b2722d68c3995c21b449cec175e84a04e547d983
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2024-10-04 00:57:37 +0100

    Add clips and stories

---
+++ conteudos_rapidos/LLM.py
@@ -0,0 +1,89 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import InferenceClient
+load_dotenv()
+
+
+# MODEL = "meta-llama/Meta-Llama-3.1-70B-Instruct"
+MODEL = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+KEY = os.getenv("HF_KEY")
+
+class LLM:
+    """
+    A class to interact with the LLM model
+    """
+
+    def __init__(
+        self, 
+        model=MODEL, 
+        key=KEY
+    ):
+        self.model = model
+        self.key = key
+
+    
+    def query_cloud(self, query: str) -> str:
+        """
+        Query the model on the cloud
+
+        Args:
+            query (str): The query to ask the model
+
+        Returns:
+            str: The response from the model
+        """
+        client = InferenceClient(api_key=self.key)
+        messages = [
+	        { "role": "user", "content": query }
+        ]
+        output = client.chat.completions.create(
+            model=self.model,
+            messages=messages, 
+            stream=True, 
+            temperature=0.5,
+            max_tokens=1024,
+            top_p=0.7
+        )
+        answer = ''.join(
+            chunk.choices[0].delta.content for chunk in output
+        )
+        return answer
+    
+
+    def transcript_to_query(self, transcripts: list) -> str:
+        """
+        Convert a list of transcripts to a query
+
+        Args:
+            transcripts (list): A list of transcripts
+
+        Returns:
+            str: The query to ask the model
+        """
+        text = " ".join(transcripts)
+        query = (
+            "Preciso de um titulo chamativo, resumo em formato expositivo e 3 keywords para a seguinte transcrição de um clipe de um programa de TV:\n\n" +  
+            f"{text}\n\n" +
+            "A resposta deve ser em português de portugal e ter o seguinte formato: \n Titulo: ... \n Resumo: ... \n Keywords: ..."
+        )
+        return query
+    
+
+    def process_transcripts(self, transcripts: list) -> tuple[str, str, list[str]]:
+        """
+        Process a list of transcripts
+
+        Args:
+            transcripts (list): A list of transcripts
+
+        Returns:
+            Tuple[str, str, List[str]]: Title, summary, and keywords for the transcripts
+        """
+        query = self.transcript_to_query(transcripts)
+        response = self.query_cloud(query)
+        response = response.split("\n")
+        response = [line.strip() for line in response if line]
+        title = response[0][8:]
+        summary = response[1][8:]
+        keywords = response[2][10:].split(",")
+        return title, summary, keywords
---
+++ conteudos_rapidos/clips.py
@@ -0,0 +1,60 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+
+BASE_URL = "https://mediadive.poc.alticelabs.com/pubblocks"
+API_KEY = os.getenv("API_KEY")
+STATION_ID = 327250
+
+
+def clips_details(station_id, start_time, end_time):
+    url = f"{BASE_URL}/topics?stationId={station_id}&startTime={start_time}&endTime={end_time}"
+    header = {
+        "Authorization": f"Basic {API_KEY}",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36"
+    }
+    response = requests.get(url, headers=header)
+    response = response.json()
+
+    return response['topicSegmentGroup']
+
+
+if __name__ == "__main__":
+
+    # load profile
+    with open('../data/david_example.json') as f:
+        profile = json.load(f)
+
+    preferences = profile['preferences']
+    programs = profile['programs']
+
+    # get clips of each program
+    clips = []
+    for program in programs:
+        startTime = program['StartDate']
+        endTime = program['EndDate']
+
+        details = clips_details(STATION_ID, startTime, endTime)
+
+        for clip in details:
+            # ignore very short clips
+            if clip['n_segments'] < 2:
+                continue
+
+            description = clip['description']
+            if description in preferences:
+                clip['rank'] = preferences[description]
+            else:
+                clip['rank'] = 0
+
+            clips.append(clip)
+
+    # sort clips by rank
+    clips.sort(key=lambda x: x['rank'], reverse=True)
+
+    # save clips as json file
+    with open("clips.json", 'w') as f:
+        json.dump(clips, f, indent=4)
